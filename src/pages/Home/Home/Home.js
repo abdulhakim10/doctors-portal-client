e52010690398245a5aa6387c8c92e71dd2f2654d
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-11-15 12:49:45 +0600

    Responsive Grid for services section

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import InfoCards from '../InfoCards/InfoCards';
+import Services from '../Services/Services';
+import ExceptionalDental from './ExceptionalDental/ExceptionalDental';
 
 const Home = () => {
     return (
         <div className='mx-5'>
            <Banner></Banner>
            <InfoCards></InfoCards>
+           <Services></Services>
+           <ExceptionalDental></ExceptionalDental>
         </div>
     );
 };
