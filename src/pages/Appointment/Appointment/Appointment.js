b14313ac72f5735e609ee63d02f4b7ab99091e8d
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-11-17 01:04:50 +0600

    calendar added on AppointmentBanner

---
+++ src/pages/Appointment/Appointment/Appointment.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppointmentBanner from '../AppointmentBanner/AppointmentBanner';
+
+const Appointment = () => {
+    return (
+        <div>
+            <h2 className="text-2xl">This is Appointment</h2>
+            <AppointmentBanner></AppointmentBanner>
+        </div>
+    );
+};
+
+export default Appointment;
--- src/pages/Home/MakeAppointment/MakeAppointment.js
+++ src/pages/Home/MakeAppointment/MakeAppointment.js
@@ -13,8 +13,8 @@ const MakeAppointment = () => {
             <div className="hero-content flex-col lg:flex-row mt-12">
                 <img src={doctor} alt='' className="-mt-40 hidden md:block lg:w-1/2 rounded-lg " />
                 <div className='text-white'>
-                    <h1 className="text-5xl font-bold">Box Office News!</h1>
-                    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <h1 className="text-5xl font-bold">Make an appointment Today</h1>
+                    <p className="py-6">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
                     <PrimaryButton>Appointment</PrimaryButton>
                 </div>
             </div>
