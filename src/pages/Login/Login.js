72527d04d4c16efcd6abf1e57850070e0f7be8f9
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-11-17 15:11:52 +0600

    Setup Login page, and Firebase Project setup

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     const { register, handleSubmit } = useForm();
-    const [data, setData] = useState('')
+    
+    const handleLogin = data => {
+        console.log(data)
+    }
     return (
         <div className='h-[800px] flex justify-center items-center'>
-            <div>
-                <h2 className='text-4xl font-bold'>Login</h2>
-                <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+            <div className='w-96 p-7'>
+                <h2 className='text-4xl font-bold text-center'>Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"><span className="label-text">Email</span></label>
                         <input type="email" {...register("email")}  className="input input-bordered w-full max-w-xs" />
@@ -16,11 +20,13 @@ const Login = () => {
                     <div className="form-control w-full max-w-xs">
                         <label className="label"><span className="label-text">Password</span></label>
                         <input type="password" {...register("password")}  className="input input-bordered w-full max-w-xs" />
+                        <label className="label"><span className="label-text">Forget Password?</span></label>
                     </div>
-                    
-                    <p>{data}</p>
-                    <input type="submit" />
+                    <input className='btn btn-accent w-full max-w-xs' value='Login' type="submit" />
                 </form>
+                <p>New to Doctors Portal? <Link className='text-secondary' to='/signup'>Create new account</Link></p>
+                <div className="divider">OR</div>
+                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
             </div>
         </div>
     );
