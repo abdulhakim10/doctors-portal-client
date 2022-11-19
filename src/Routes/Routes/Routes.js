c02353f010c3720fe48d807dd3bc95d08bbd4f76
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-11-20 02:15:50 +0600

    Display User specific Appointments using Data Table

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import DashboardLayout from "../../Layout/DashboardLayout";
 import Main from "../../Layout/Main";
 import Appointment from "../../pages/Appointment/Appointment/Appointment";
-import Dashboard from "../../pages/Dashboard/Dashboard/Dashboard";
+import MyAppointment from "../../pages/Dashboard/MyAppointment/MyAppointment";
 import Home from "../../pages/Home/Home/Home";
 import Login from "../../pages/Login/Login";
 import Signup from "../../pages/Signup/Signup";
@@ -37,7 +37,7 @@ const router = createBrowserRouter([
         children: [
             {
                 path: '/dashboard',
-                element: <Dashboard></Dashboard>
+                element: <MyAppointment></MyAppointment>
             }
         ]
     }
